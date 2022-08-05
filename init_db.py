a881ccdcc9fe10a79f630259331dd125bfc7bdb3
Matt Davis <matteius@gmail.com>
2022-08-05 00:38:49 -0400

    Define the response models of the API.

--- init_db.py
+++ init_db.py
@@ -6,7 +6,7 @@ from opensensor.utils import get_open_sensor_db
 
 db = get_open_sensor_db()
 try:
-    db.validate_collection("Temperature")  # Try to validate a collection
+    db.Temperature  # Try to validate a collection
 except pymongo.errors.OperationFailure:  # If the collection doesn't exist
     print("Temperature collection doesn't exist yet; Creating ...")
     db.create_collection(
@@ -14,7 +14,7 @@ except pymongo.errors.OperationFailure:  # If the collection doesn't exist
         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
     )
 try:
-    db.validate_collection("Humidity")  # Try to validate a collection
+    db.Humidity  # Try to validate a collection
 except pymongo.errors.OperationFailure:  # If the collection doesn't exist
     print("Humidity collection doesn't exist yet; Creating ...")
     db.create_collection(
--- main.py
+++ main.py
@@ -23,14 +23,40 @@ class Temperature(BaseModel):
     temp: Decimal
     unit: str
 
+    @classmethod
+    def parse_obj_from_ts(cls, ts_data):
+        metadata = ts_data.pop("metadata", {})
+        ts_data["unit"] = metadata.get("unit")
+        return cls.parse_obj(ts_data)
+
+
+class TemperatureMeta(BaseModel):
+    metadata: SensorMetaData
+    temp: Temperature
+
+
+class TemperaturesMeta(BaseModel):
+    metadata: SensorMetaData
+    results: list[Temperature]
+
 
 class Humidity(BaseModel):
     rh: Decimal
 
 
+class HumidityMeta(BaseModel):
+    metadata: SensorMetaData
+    rh: Humidity
+
+
 class Environment(BaseModel):
     temp: Temperature | None = None
     rh: Humidity | None = None
+
+
+class EnvironmentMeta(BaseModel):
+    metadata: SensorMetaData
+    environment: Environment
 
 
 def _get_sensor_ts_metadata(metadata: SensorMetaData):
@@ -62,32 +88,35 @@ def _record_temperature_data(metadata: SensorMetaData, temp: Temperature):
     temps.insert_one(temp_data)
 
 
-@app.post("/rh/")
+@app.post("/rh/", response_model=HumidityMeta)
 async def record_humidity(metadata: SensorMetaData, rh: Humidity):
     _record_humidity_data(metadata, rh)
     return {"metadata": metadata, "rh": rh}
 
 
-@app.post("/temp/")
+@app.post("/temp/", response_model=TemperatureMeta)
 async def record_temperature(metadata: SensorMetaData, temp: Temperature):
     _record_temperature_data(metadata, temp)
     return {"metadata": metadata, "temp": temp}
 
 
-@app.get("/temp/{device_id}")
+@app.get("/temp/{device_id}", response_model=TemperaturesMeta)
 async def historical_temperatures(
     device_id: str = Path(title="The ID of the device about which to retrieve historical data."),
 ):
     db = get_open_sensor_db()
-    temps = db.defaultTemp
-    results = temps.find({"metadata.device_id": device_id}, {"_id": 0})
-    data = []
-    for temp in results:
-        data.append(temp)
+    temps = db.Temperature
+    matching_data = temps.find(
+        {"metadata.device_id": device_id}, {"_id": 0, "metadata.device_id": 0, "metadata.name": 0}
+    )
+    results = []
+    data = {"metadata": {"device_id": device_id}, "results": results}
+    for temp in matching_data:
+        results.append(Temperature.parse_obj_from_ts(temp))
     return data
 
 
-@app.post("/environment/")
+@app.post("/environment/", response_model=EnvironmentMeta)
 async def record_environment(metadata: SensorMetaData, environment: Environment):
     if environment.temp:
         _record_temperature_data(metadata, environment.temp)
