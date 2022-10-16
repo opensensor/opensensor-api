cc08a62e1f7864ff94805eac6a1e80f3ab5abbdc
Matt Davis <matteius@gmail.com>
2022-10-16 00:32:38 -0400

    Simplify API with pagination.

--- main.py
+++ main.py
@@ -24,27 +24,13 @@ class SensorMetaData(BaseModel):
 
 
 class Temperature(BaseModel):
+    metadata: SensorMetaData
     temp: Decimal
-    metadata: SensorMetaData | None = None
-
-
-class TemperatureMeta(BaseModel):
-    metadata: SensorMetaData
-    temp: Temperature
-
-
-class TemperaturesMeta(BaseModel):
-    metadata: SensorMetaData
-    results: list[Temperature]
 
 
 class Humidity(BaseModel):
     rh: Decimal
-
-
-class HumidityMeta(BaseModel):
-    metadata: SensorMetaData
-    rh: Humidity
+    metadata: SensorMetaData | None = None
 
 
 class Environment(BaseModel):
@@ -52,43 +38,38 @@ class Environment(BaseModel):
     rh: Humidity | None = None
 
 
-class EnvironmentMeta(BaseModel):
-    metadata: SensorMetaData
-    environment: Environment
-
-
-def _record_humidity_data(metadata: SensorMetaData, rh: Humidity):
+def _record_humidity_data(rh: Humidity):
     db = get_open_sensor_db()
     rhs = db.Humidity
     humidity_data = {
         "timestamp": datetime.utcnow(),
-        "metadata": metadata.dict(),
+        "metadata": rh.metadata.dict(),
         "rh": str(rh.rh),
     }
     rhs.insert_one(humidity_data)
 
 
-def _record_temperature_data(metadata: SensorMetaData, temp: Temperature):
+def _record_temperature_data(temp: Temperature):
     db = get_open_sensor_db()
     temps = db.Temperature
     temp_data = {
         "timestamp": datetime.utcnow(),
-        "metadata": metadata.dict(),
+        "metadata": temp.metadata.dict(),
         "temp": str(temp.temp),
     }
     temps.insert_one(temp_data)
 
 
-@app.post("/rh/", response_model=HumidityMeta)
+@app.post("/rh/", response_model=Humidity)
 async def record_humidity(metadata: SensorMetaData, rh: Humidity):
-    _record_humidity_data(metadata, rh)
+    _record_humidity_data(rh)
     return {"metadata": metadata, "rh": rh}
 
 
-@app.post("/temp/", response_model=TemperatureMeta)
-async def record_temperature(metadata: SensorMetaData, temp: Temperature):
-    _record_temperature_data(metadata, temp)
-    return {"metadata": metadata, "temp": temp}
+@app.post("/temp/", response_model=Temperature)
+async def record_temperature(temp: Temperature):
+    _record_temperature_data(temp)
+    return {"temp": temp}
 
 
 @app.get("/temp/{device_id}", response_model=Page[Temperature])
@@ -104,13 +85,13 @@ async def historical_temperatures(
     return matching_data
 
 
-@app.post("/environment/", response_model=EnvironmentMeta)
-async def record_environment(metadata: SensorMetaData, environment: Environment):
+@app.post("/environment/", response_model=Environment)
+async def record_environment(environment: Environment):
     if environment.temp:
-        _record_temperature_data(metadata, environment.temp)
+        _record_temperature_data(environment.temp)
     if environment.rh:
-        _record_humidity_data(metadata, environment.rh)
-    return {"metadata": metadata, "environment": environment}
+        _record_humidity_data(environment.rh)
+    return {"environment": environment}
 
 
 add_pagination(app)
