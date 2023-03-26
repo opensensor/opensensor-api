81d38d5f9582ed883810876798ca6c42e46475a4
Matt Davis <matteius@gmail.com>
2023-03-26 17:39:34 -0400

    Fix Kalvin conversions.

--- main.py
+++ main.py
@@ -212,11 +212,11 @@ def convert_temperature(temp: Temperature, desired_unit: str) -> Decimal:
     elif temp.unit == "F" and desired_unit == "C":
         temp.temp = Decimal((temp.temp - 32) * 5 / 9)
     elif temp.unit == "F" and desired_unit == "K":
-        temp.temp = Decimal((temp.temp + 459.67) * 5 / 9)
+        temp.temp = Decimal((temp.temp + Decimal(459.67)) * 5 / 9)
     elif temp.unit == "K" and desired_unit == "C":
-        temp.temp = Decimal(temp.temp - 273.15)
+        temp.temp = Decimal(temp.temp - Decimal(273.15))
     elif temp.unit == "K" and desired_unit == "F":
-        temp.temp = Decimal(temp.temp * 9 / 5 - 459.67)
+        temp.temp = Decimal(temp.temp * 9 / 5 - Decimal(459.67))
     else:
         raise ValueError(f"Unsupported temperature unit conversion: {temp.unit} to {desired_unit}")
     temp.unit = desired_unit
