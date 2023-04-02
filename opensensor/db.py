244b5f2f1b2d895dc90dca1b08fed0ebc86fdfbe
Matt Davis <matteius@gmail.com>
2023-04-01 20:59:06 -0400

    fix lint

--- opensensor/db.py
+++ opensensor/db.py
@@ -1,16 +1,6 @@
 import os
-import uuid
 
-import motor.motor_asyncio
-from pydantic import Field
 from pymongo import MongoClient
-
-
-def get_motor_mongo_connection():
-    connection_str = os.environ.get("OPENSENSOR_DB") or ""
-    client = motor.motor_asyncio.AsyncIOMotorClient(connection_str, uuidRepresentation="standard")
-    db = client["default"]
-    return db
 
 
 def get_mongo_connection():
@@ -23,4 +13,3 @@ def get_open_sensor_db():
     client = get_mongo_connection()
     db = client["default"]
     return db
-
