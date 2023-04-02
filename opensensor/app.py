cdfad4add8f9e39bd088ca0c2dcba6ee60682707
Matt Davis <matteius@gmail.com>
2023-04-02 07:56:13 -0400

    fix build

--- opensensor/app.py
+++ opensensor/app.py
@@ -1,21 +1,13 @@
 import datetime
 import json
 
-from fastapi import Body, Depends, FastAPI, Query, Request, Response
+from fastapi import Body, Depends, FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import RedirectResponse
 from fief_client import FiefAccessTokenInfo
 from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
-from opensensor.users import (
-    SESSION_COOKIE_NAME,
-    add_api_key,
-    auth,
-    fief,
-    get_or_create_user,
-    get_redirect_uri,
-)
+from opensensor.users import add_api_key, auth, get_or_create_user
 
 origins = [
     "https://graph.opensensor.io",
@@ -46,23 +38,6 @@ app.add_middleware(
 @app.get("/health")
 async def health_check():
     return {"status": "OK"}
-
-
-@app.get("/auth-callback", name="auth_callback")
-async def auth_callback(request: Request, response: Response, code: str = Query(...)):
-    redirect_uri = get_redirect_uri(request)
-    tokens, _ = await fief.auth_callback(code, redirect_uri)
-
-    response = RedirectResponse(request.url_for("protected"))
-    response.set_cookie(
-        SESSION_COOKIE_NAME,
-        tokens["access_token"],
-        max_age=tokens["expires_in"],
-        httponly=True,
-        secure=False,
-    )
-
-    return response
 
 
 @app.post("/generate-api-key")
