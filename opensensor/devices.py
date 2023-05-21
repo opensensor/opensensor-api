00dc186dc94a29aac7fa489efea52705148e688a
Matt Davis <matteius@gmail.com>
2023-05-21 16:43:02 -0400

    Add device API for consuming the command.

--- opensensor/devices.py
+++ opensensor/devices.py
@@ -2,6 +2,7 @@ from bson import Binary
 from fastapi import APIRouter, Body, Depends, Response, status
 from fief_client import FiefAccessTokenInfo
 
+from opensensor.collections import DeviceMetadata
 from opensensor.db import get_open_sensor_db
 from opensensor.users import (
     Command,
@@ -9,10 +10,8 @@ from opensensor.users import (
     auth,
     device_id_is_allowed_for_user,
     get_or_create_user,
+    validate_device_metadata,
 )
-
-# device_id -> common_name -> matching_commands
-
 
 router = APIRouter()
 
@@ -42,6 +41,32 @@ def add_command_for_user(user: User, device_id: str, device_name: str, command: 
     return new_command
 
 
+def consume_next_command_for_device(user: User, device_name: str):
+    db = get_open_sensor_db()
+    users_db = db["Users"]
+
+    # Find the command for the device
+    command = None
+    for command in user.commands_issued:
+        if command.device_name == device_name:
+            break
+
+    if command is None:
+        return None
+
+    # Remove the command from the user's list
+    user.commands_issued.remove(command)
+
+    # Convert the Command instances in the commands_issued list to dictionaries
+    commands_list = [command.dict() for command in user.commands_issued]
+
+    users_db.update_one(
+        {"_id": Binary.from_uuid(user.fief_user_id)}, {"$set": {"commands_issued": commands_list}}
+    )
+
+    return command
+
+
 @router.post("/command/device")
 async def issue_command(
     device_id: str = Body(...),
@@ -60,3 +85,13 @@ async def issue_command(
     add_command_for_user(user, device_id, device_name, command)
 
     return Response(status_code=status.HTTP_201_CREATED)
+
+
+@router.post("/command/consume")
+async def consume_command(
+    device_metadata: DeviceMetadata,
+    user: User = Depends(validate_device_metadata),
+):
+    command = consume_next_command_for_device(user, device_metadata.name)
+
+    return Response(command.dict(), status_code=status.HTTP_201_CREATED)
