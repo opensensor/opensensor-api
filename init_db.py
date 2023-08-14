12df698dabf7fefb90aca6f82ee4bd4c098cccf1
Matt Davis <matteius@gmail.com>
2023-08-14 05:44:14 -0400

    Revert "Begin to factor towards a single data collection (#75)"
    This reverts commit 1c4cf21255c073dc4e9c396716d99091213ee135.

--- init_db.py
+++ init_db.py
@@ -1,83 +1,62 @@
 import pymongo
 
-from opensensor.db import get_open_sensor_db
+from opensensor.utils import get_open_sensor_db
 
 # Script for creating the Time Series
 
 db = get_open_sensor_db()
-# try:
-#     db.create_collection(
-#         "Temperature",
-#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-#     )
-# except pymongo.errors.CollectionInvalid:
-#     print("Temperature collection Already exists, skipping ...")
-#
-# try:
-#     db.create_collection(
-#         "Humidity",
-#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-#     )
-# except pymongo.errors.CollectionInvalid:
-#     print("Humidity collection Already exists, skipping ...")
-#
-# try:
-#     db.create_collection(
-#         "Pressure",
-#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-#     )
-# except pymongo.errors.CollectionInvalid:
-#     print("Pressure collection Already exists, skipping ...")
-#
-# try:
-#     db.create_collection(
-#         "Lux",
-#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-#     )
-# except pymongo.errors.CollectionInvalid:
-#     print("Lux collection Already exists, skipping ...")
-#
-# try:
-#     db.create_collection(
-#         "CO2",
-#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-#     )
-# except pymongo.errors.CollectionInvalid:
-#     print("CO2 collection Already exists, skipping ...")
-#
-# try:
-#     db.create_collection(
-#         "Moisture",
-#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-#     )
-# except pymongo.errors.CollectionInvalid:
-#     print("Moisture collection Already exists, skipping ...")
-#
-# try:
-#     db.create_collection(
-#         "pH",
-#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-#     )
-# except pymongo.errors.CollectionInvalid:
-#     print("pH collection Already exists, skipping ...")
-
 try:
     db.create_collection(
-        "FreeTier",
+        "Temperature",
         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-        # expireAfterSeconds=8000000,
     )
 except pymongo.errors.CollectionInvalid:
-    print("FreeTier collection Already exists, skipping ...")
-
+    print("Temperature collection Already exists, skipping ...")
 
 try:
     db.create_collection(
-        "Migration",
-        # timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-        # expireAfterSeconds=8000000,
+        "Humidity",
+        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
     )
-    db.Migration.create_index([("migration_name", 1)], unique=True)
-
 except pymongo.errors.CollectionInvalid:
-    print("Migration collection Already exists, skipping ...")
+    print("Humidity collection Already exists, skipping ...")
+
+try:
+    db.create_collection(
+        "Pressure",
+        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+    )
+except pymongo.errors.CollectionInvalid:
+    print("Pressure collection Already exists, skipping ...")
+
+try:
+    db.create_collection(
+        "Lux",
+        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+    )
+except pymongo.errors.CollectionInvalid:
+    print("Lux collection Already exists, skipping ...")
+
+try:
+    db.create_collection(
+        "CO2",
+        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+    )
+except pymongo.errors.CollectionInvalid:
+    print("CO2 collection Already exists, skipping ...")
+
+try:
+    db.create_collection(
+        "Moisture",
+        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+    )
+except pymongo.errors.CollectionInvalid:
+    print("Moisture collection Already exists, skipping ...")
+
+try:
+    db.create_collection(
+        "pH",
+        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+    )
+except pymongo.errors.CollectionInvalid:
+    print("pH collection Already exists, skipping ...")
