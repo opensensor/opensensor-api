dc051a019b130454026eba8ae46fbc2bc2c44fa5
Matt Davis <matteius@gmail.com>
2023-08-14 05:44:53 -0400

    Revert "Revert "Begin to factor towards a single data collection (#75)" (#76)"
    This reverts commit ef111bd32444f0360d86f0f7cea2b34d089679b7.

--- init_db.py
+++ init_db.py
@@ -1,62 +1,83 @@
 import pymongo
 
-from opensensor.utils import get_open_sensor_db
+from opensensor.db import get_open_sensor_db
 
 # Script for creating the Time Series
 
 db = get_open_sensor_db()
-try:
-    db.create_collection(
-        "Temperature",
-        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-    )
-except pymongo.errors.CollectionInvalid:
-    print("Temperature collection Already exists, skipping ...")
+# try:
+#     db.create_collection(
+#         "Temperature",
+#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+#     )
+# except pymongo.errors.CollectionInvalid:
+#     print("Temperature collection Already exists, skipping ...")
+#
+# try:
+#     db.create_collection(
+#         "Humidity",
+#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+#     )
+# except pymongo.errors.CollectionInvalid:
+#     print("Humidity collection Already exists, skipping ...")
+#
+# try:
+#     db.create_collection(
+#         "Pressure",
+#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+#     )
+# except pymongo.errors.CollectionInvalid:
+#     print("Pressure collection Already exists, skipping ...")
+#
+# try:
+#     db.create_collection(
+#         "Lux",
+#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+#     )
+# except pymongo.errors.CollectionInvalid:
+#     print("Lux collection Already exists, skipping ...")
+#
+# try:
+#     db.create_collection(
+#         "CO2",
+#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+#     )
+# except pymongo.errors.CollectionInvalid:
+#     print("CO2 collection Already exists, skipping ...")
+#
+# try:
+#     db.create_collection(
+#         "Moisture",
+#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+#     )
+# except pymongo.errors.CollectionInvalid:
+#     print("Moisture collection Already exists, skipping ...")
+#
+# try:
+#     db.create_collection(
+#         "pH",
+#         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+#     )
+# except pymongo.errors.CollectionInvalid:
+#     print("pH collection Already exists, skipping ...")
 
 try:
     db.create_collection(
-        "Humidity",
+        "FreeTier",
         timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+        # expireAfterSeconds=8000000,
     )
 except pymongo.errors.CollectionInvalid:
-    print("Humidity collection Already exists, skipping ...")
+    print("FreeTier collection Already exists, skipping ...")
+
 
 try:
     db.create_collection(
-        "Pressure",
-        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+        "Migration",
+        # timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
+        # expireAfterSeconds=8000000,
     )
-except pymongo.errors.CollectionInvalid:
-    print("Pressure collection Already exists, skipping ...")
+    db.Migration.create_index([("migration_name", 1)], unique=True)
 
-try:
-    db.create_collection(
-        "Lux",
-        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-    )
 except pymongo.errors.CollectionInvalid:
-    print("Lux collection Already exists, skipping ...")
-
-try:
-    db.create_collection(
-        "CO2",
-        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-    )
-except pymongo.errors.CollectionInvalid:
-    print("CO2 collection Already exists, skipping ...")
-
-try:
-    db.create_collection(
-        "Moisture",
-        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-    )
-except pymongo.errors.CollectionInvalid:
-    print("Moisture collection Already exists, skipping ...")
-
-try:
-    db.create_collection(
-        "pH",
-        timeseries={"timeField": "timestamp", "metaField": "metadata", "granularity": "minutes"},
-    )
-except pymongo.errors.CollectionInvalid:
-    print("pH collection Already exists, skipping ...")
+    print("Migration collection Already exists, skipping ...")
---
+++ migrate_collections.py
@@ -0,0 +1,100 @@
+from datetime import datetime, timedelta
+from operator import itemgetter
+
+from pymongo import ASCENDING
+
+from opensensor.collection_apis import new_collections, old_collections
+
+# Create a MongoDB client
+from opensensor.db import get_open_sensor_db
+
+# Access the database
+db = get_open_sensor_db()
+
+# List of all collections/models to migrate
+collections_to_migrate = ["Temperature", "Humidity", "Pressure", "Lux", "CO2", "PH", "Moisture"]
+
+migration = db.Migration.find_one({"migration_name": "FreeTier"})
+if not migration:
+    db["Migration"].insert_one({"migration_name": "FreeTier", "migration_complete": False})
+
+# Determine the earliest and latest timestamps in your data
+earliest_timestamp = datetime.now()
+latest_timestamp = datetime.min
+
+for collection_name in collections_to_migrate:
+    collection = db[collection_name]
+    earliest_document = collection.find_one(sort=[("timestamp", ASCENDING)])
+    latest_document = collection.find_one(sort=[("timestamp", -1)])
+    if earliest_document and earliest_document["timestamp"] < earliest_timestamp:
+        earliest_timestamp = earliest_document["timestamp"]
+    if latest_document and latest_document["timestamp"] > latest_timestamp:
+        latest_timestamp = latest_document["timestamp"]
+
+# Migrate data in chunks, e.g., one week at a time
+start_date = earliest_timestamp
+one_week = timedelta(weeks=1)
+
+while start_date <= latest_timestamp:
+    end_date = start_date + one_week
+    all_documents = []
+
+    # Define a "reasonable" time window
+    reasonable_time = timedelta(seconds=3)
+
+    # Migrate data in chunks
+    start_date = earliest_timestamp
+
+    while start_date <= latest_timestamp:
+        end_date = start_date + one_week
+        buffer = {}  # We'll store the records for the current time window here
+
+        for collection_name in collections_to_migrate:
+            collection = db[collection_name]
+            for document in collection.find({"timestamp": {"$gte": start_date, "$lt": end_date}}):
+                # Convert to the FreeTier model
+                unit = document["metadata"].get("unit")
+                new_document = {
+                    "metadata": {
+                        "device_id": document["metadata"]["device_id"],
+                        "name": document["metadata"].get("name"),
+                        "user_id": document.get("user_id"),
+                    },
+                    new_collections[collection_name]: document.get(
+                        old_collections[collection_name]
+                    ),
+                    "timestamp": document["timestamp"],
+                }
+                if unit:
+                    new_document[f"{new_collections[collection_name]}_unit"] = unit
+
+                # Merge with an existing document if it's within a reasonable time,
+                # otherwise add a new document to the buffer
+                for existing_timestamp in buffer.keys():
+                    if abs(existing_timestamp - document["timestamp"]) <= reasonable_time:
+                        buffer[existing_timestamp][new_collections[collection_name]] = document.get(
+                            old_collections[collection_name]
+                        )
+                        if unit:
+                            buffer[existing_timestamp][
+                                f"{new_collections[collection_name]}_unit"
+                            ] = unit
+                        break
+                else:
+                    buffer[document["timestamp"]] = new_document
+
+        # Sort all documents by timestamp
+        all_documents = sorted(buffer.values(), key=itemgetter("timestamp"))
+
+        # Access the destination collection
+        free_tier_collection = db["FreeTier"]
+
+        # Insert all documents into the new collection, in sorted order
+        for document in all_documents:
+            free_tier_collection.insert_one(document)
+
+        # Advance to the next time chunk
+        start_date = end_date
+
+
+db["Migration"].update_one({"migration_name": "FreeTier"}, {"$set": {"migration_complete": True}})
